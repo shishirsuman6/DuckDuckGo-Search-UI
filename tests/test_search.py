e6c14dcb2410cbe5dabc6b2ebeda840e82b0058d
shishirsuman6 <shishirsuman@aol.com>
2022-05-17 21:49:22 -0400

    added parameterized test runs

--- tests/test_search.py
+++ tests/test_search.py
@@ -14,31 +14,31 @@ from pages.search import DuckDuckGoSearchPage
 # Tests
 #--------------------------------------------------------------------
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['NFL','NBA','MLB'])
+def test_basic_duckduckgo_search(browser, phrase):
 
     # Scenario: Basic DuckDuckGo search
     search_page= DuckDuckGoSearchPage(browser)
     result_page= DuckDuckGoResultPage(browser)
-    PHRASE='orange'
-
+    
     # Given the DuckDuckGo Home page is displayed
     search_page.load()
     
-    # When the user searches for "panda"
-    search_page.search(PHRASE)
+    # When the user searches for phrase
+    search_page.search(phrase)
     
     
     
-    # And the search result query is "panda"
-    assert PHRASE == result_page.search_input_value()
+    # And the search result query is phrase
+    assert phrase == result_page.search_input_value()
     
-    # And the search result links pertain to "panda"
+    # And the search result links pertain to phrase
     titles = result_page.result_link_titles()
-    matches = [t for t in titles if PHRASE.lower() in t.lower() ]
+    matches = [t for t in titles if phrase.lower() in t.lower() ]
     assert len(matches) > 0
 
-    # Then the search result title contains "panda"
-    assert PHRASE in result_page.title()
+    # Then the search result title contains phrase
+    assert phrase in result_page.title()
     
     # Remove this once the test is complete
     # raise Exception("Incomplete Test")
